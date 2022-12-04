4cd38d5473d8d126758d90dac97603ef3235b4ea
1010504 <112973177+Yuriumi@users.noreply.github.com>
2022-12-04 20:17:52 +0800

    12-添加地面检测

---
+++ Assets/Scripts/Player/OnGroundSensor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGroundSensor : MonoBehaviour
+{
+    public CapsuleCollider capcol;
+
+    private Vector3 point1;
+    private Vector3 point2;
+    public float radius = 0.1f;
+
+    private Collider[] outPutCols = new Collider[1];
+    public LayerMask groundMask;
+    private bool IsOnGrounded => Physics.OverlapSphereNonAlloc(transform.position, radius, outPutCols, groundMask) != 0;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (IsOnGrounded)
+        {
+            Debug.Log("OnGround");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
