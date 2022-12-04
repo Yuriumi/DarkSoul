297861c56c808d83201919660ab2cc24295b10fe
1010504 <112973177+Yuriumi@users.noreply.github.com>
2022-12-04 14:51:18 +0800

    06-连接动画控制模块与输入模块

---
+++ Assets/Scripts/AnimationSystem/ActorControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorControl : MonoBehaviour
+{
+    public GameObject playerModel;
+    public PlayerInput playerInput;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        animator = playerModel.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("forward", playerInput.Dup);
+    }
+}
