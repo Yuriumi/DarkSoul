b08cd941ee70821a493c43b4e512427f001ba24c
1010504 <112973177+Yuriumi@users.noreply.github.com>
2022-12-04 14:11:30 +0800

    02-平滑输入与开关输入
    1.平滑输入
    2.设置输入模块的开关

--- Assets/Scripts/InputSystem/PlayerInput.cs
+++ Assets/Scripts/InputSystem/PlayerInput.cs
@@ -11,6 +11,15 @@ public class PlayerInput : MonoBehaviour
 
     public float Dup;
     public float Dright;
+
+    private float targetDup;
+    private float targetDright;
+    private float velocityDup;
+    private float velocityDright;
+
+    public float smoothTime = 0.2f;
+
+    public bool inputEnable = true;
     private void Start()
     {
         
@@ -18,7 +27,16 @@ public class PlayerInput : MonoBehaviour
 
     private void Update()
     {
-        Dup = (Input.GetKey(keyUp)? 1.0f:0) - (Input.GetKey(keyDown)? 1.0f : 0);
-        Dright = (Input.GetKey(keyLeft)? 1.0f:0) - (Input.GetKey(keyRight)? 1.0f : 0);
+        targetDup = (Input.GetKey(keyUp)? 1.0f:0) - (Input.GetKey(keyDown)? 1.0f : 0);
+        targetDright = (Input.GetKey(keyLeft)? 1.0f:0) - (Input.GetKey(keyRight)? 1.0f : 0);
+
+        if (inputEnable == false)
+        {
+            targetDup = 0f;
+            targetDright = 0f;
+        }
+
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, smoothTime);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, smoothTime);
     }
 }
